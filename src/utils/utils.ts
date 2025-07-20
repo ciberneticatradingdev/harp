803a5888e21381fb888c4c507ead14c403638064
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-07-20 06:36:52 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,15 @@
-export const CA = "9FzeRFVq5weQHTPRE8k1LA1etxU2TqA1vp71EBxYpump";
-export const x ='https://x.com/';
-export const tg = 'https://t.me/ZoomieOnSolPortal';
+// Contract Address
+export const CA = "ca";
+
+// Social Media Links
+export const TWITTER_URL = "https://x.com/i/";
+export const TIKTOK_URL = "https://www.tiktok.com/";
+export const TELEGRAM_URL = "https://t.me/";
+
+// Trading Links
+export const BUY_URL = `https://letsbonk.fun/token/${CA}`;
+export const DEXSCREENER_URL = `https://dexscreener.com/solana/${CA}`;
+
+// Legacy exports (mantener compatibilidad)
+export const x = 'https://x.com/';
+export const tg = 'https://t.me/';
