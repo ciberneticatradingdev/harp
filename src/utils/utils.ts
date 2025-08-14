c22ea006e4d470265bfdff779a9e9549d435f662
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-08-14 12:10:30 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,17 @@
-// Contract Address
-export const CA = "DvTjjC5GnXmqjrps8DnYPK82BApe9eWUQjbSKt8bonk";
+// Money Buys Happiness - Memecoin Configuration
+
+// Contract Address (will be updated when deployed)
+export const CA = "CA_WILL_UPDATE_SOON";
 
 // Social Media Links
-export const TWITTER_URL = "https://x.com/i/communities/1947661023051219390";
-export const TIKTOK_URL = "https://x.com/i/communities/1947661023051219390";
-export const TELEGRAM_URL = "https://x.com/i/communities/1947661023051219390";
+export const TWITTER_URL = "https://x.com/MoneyBuysHappy";
+export const TELEGRAM_URL = "https://t.me/MoneyBuysHappiness";
 
 // Trading Links
-export const BUY_URL = `https://letsbonk.fun/token/${CA}`;
+export const BUY_URL = `https://pump.fun/coin/${CA}`;
 export const DEXSCREENER_URL = `https://dexscreener.com/solana/${CA}`;
 
-// Legacy exports (mantener compatibilidad)
-export const x = 'https://x.com/';
-export const tg = 'https://t.me/';
+// Project Information
+export const PROJECT_NAME = "Money Buys Happiness";
+export const PROJECT_SYMBOL = "HAPPY";
+export const PROJECT_DESCRIPTION = "The memecoin that speaks truth - money does buy happiness!";
