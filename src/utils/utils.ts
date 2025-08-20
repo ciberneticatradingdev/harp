f1252a9a65ef5fd0b44598ae2b8ffd80943e60f4
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-08-19 20:57:15 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,20 @@
-// Money Buys Happiness - Memecoin Configuration
+// Harp on Heaven $HARP - Memecoin Configuration
 
 // Contract Address (will be updated when deployed)
-export const CA = "5kuBiWVYFWMs4sYxUdJKpCS4xDRnubbzUU66wyBPV777";
+export const CA = "7vAExeo6jd3MNFHa3ycKMxBpwRXotS8WmPFfToo7j777";
 
 // Social Media Links
-export const TWITTER_URL = "https://x.com/GatesOnHeaven";
-export const TELEGRAM_URL = "https://x.com/GatesOnHeaven";
+export const TWITTER_URL = "https://x.com/i/communities/1957955650543116705";
+export const TELEGRAM_URL = "https://x.com/i/communities/1957955650543116705";
 
 // Trading Links
 export const BUY_URL = `https://pump.fun/coin/${CA}`;
 export const DEXSCREENER_URL = `https://dexscreener.com/solana/${CA}`;
 
 // Project Information
-export const PROJECT_NAME = "HappyCoin";
-export const PROJECT_SYMBOL = "HAPPY";
-export const PROJECT_DESCRIPTION = "The Solana memecoin that brings joy and prosperity to the crypto world. Join our community of happiness seekers.";
+export const PROJECT_NAME = "Harp on Heaven";
+export const PROJECT_SYMBOL = "$HARP";
+export const PROJECT_DESCRIPTION = "The divine Solana memecoin that takes you on a celestial journey. Join the heavenly community and experience the ultimate crypto adventure in paradise.";
+
+// Contract Address for JavaScript usage (same as CA but with descriptive name)
+export const CONTRACT_ADDRESS = CA;
